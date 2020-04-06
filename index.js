20d9328f1f836cf7a1ef122af848354dd70cff61
takshil <contact@cyrilclemenceau.fr>
2020-04-06 16:57:12 +0200

    Jeu du simon final

--- index.js
+++ index.js
@@ -9,28 +9,64 @@ const vm = new Vue({
     basGauche: false,
     basDroite: false,
     sequence: [],
+    tmp:[],
     squareMapping: ['hautGauche', 'hautDroite', 'basGauche', 'basDroite']
   },
   methods: {
     newGame(){
       this.sequence = [];
+      this.nextTurn();
+    },
+    nextTurn(){
       this.addNewElemToSequence();
-      this[this.sequence[0]] = true;
+      this.allGray();
+      this.playSequence(this.tmp[0]);
+    },
+    playSequence(instruction){
+      this[instruction] = true;
       setTimeout(function(){
         vm.allGray();
-      }, 1000);
+        vm.tmp.shift();
+        if (vm.tmp[0]){
+          setTimeout(function(){
+            vm.playSequence(vm.tmp[0]);
+          }, 400);          
+        }else{
+          vm.tmp = vm.sequence.slice();
+        }
+      }, 400);
     },
     selectSquare(instruction){
       console.log(instruction);
+      if (instruction === this.tmp[0]){
+        this[instruction] = true;
+        setTimeout(function(){
+          vm.allGray();
+          vm.tmp.shift();
+          if (!vm.tmp[0]){
+            setTimeout(function(){
+              vm.nextTurn();
+            },200);            
+          }
+        }, 400);
+      }else{
+        alert("perdu!");
+      }
     },
     addNewElemToSequence(){
       this.sequence.push(this.squareMapping[Math.floor(Math.random()*4)]);
+      this.tmp = this.sequence.slice();
     },
     allGray(){
       this.hautGauche = false,
       this.hautDroite = false,
       this.basGauche = false,
       this.basDroite = false
+    }
+  },
+  computed: {
+    score(){
+      return this.sequence.length === 0 ? 0 : this.sequence.length - 1;
     }
   }
 })
