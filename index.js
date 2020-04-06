3e18a7708b9354b5eb5159d33d44d70a2a039ac6
takshil <contact@cyrilclemenceau.fr>
2020-04-06 16:17:14 +0200

    Dyma formation - jeu du simon leçon 2 et 3

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 import Vue from 'vue'
 import './style.css';
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    hautGauche: false,
+    hautDroite: false,
+    basGauche: false,
+    basDroite: false,
+    sequence: [],
+    squareMapping: ['hautGauche', 'hautDroite', 'basGauche', 'basDroite']
+  },
+  methods: {
+    newGame(){
+      this.sequence = [];
+      this.addNewElemToSequence();
+      this[this.sequence[0]] = true;
+      setTimeout(function(){
+        vm.allGray();
+      }, 1000);
+    },
+    selectSquare(instruction){
+      console.log(instruction);
+    },
+    addNewElemToSequence(){
+      this.sequence.push(this.squareMapping[Math.floor(Math.random()*4)]);
+    },
+    allGray(){
+      this.hautGauche = false,
+      this.hautDroite = false,
+      this.basGauche = false,
+      this.basDroite = false
+    }
+  }
+})
